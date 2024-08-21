f2a637a3ce87b2d5660d26ba2fb8255e74ec3587
00-status <dedypips@gmail.com>
2024-08-21 12:32:15 -0600

    Can enter commands. Commands show up

--- src/Terminal/Terminal.tsx
+++ src/Terminal/Terminal.tsx
@@ -4,6 +4,9 @@ import { TerminalDirectory } from "./domain/types";
 export const Terminal = () => {
     const [currentDirectory, setCurrentDirectory] = useState<TerminalDirectory>();
 
+    const [commands, setCommands] = useState<Array<string>>([]);
+    const [currentValue, setCurrentValue] = useState<string>('');
+
     // Have an input directly below a div.
     // as commands are executed, the appear in the div.
     // When the div reaches max-height, the overflow is hidden.
@@ -12,6 +15,23 @@ export const Terminal = () => {
     //      element.scrollTop = element.scrollHeight;
 
     return <div>
-        Hello world!
+        <h1>Hello world!</h1>
+        <div>
+            {commands.map((command) => <div key={command}>{command}</div>)}
+        </div>
+        <input
+            value={currentValue}
+            onChange={(event) => {
+                const newValue = event.target.value ?? '';
+                setCurrentValue(newValue);
+            }}
+            onKeyUp={(event) => {
+                if (event.key === 'Enter' && currentValue)
+                {
+                    setCommands([...commands, currentValue]);
+                    setCurrentValue('');
+                }
+            }}
+        />
     </div>;
 };
